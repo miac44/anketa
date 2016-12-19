c0a9d89df5d736fcc2e0fe08791b8bd3b9e4d792
socnetspam@gmail.com <bigazzzz@gmail.com>
2016-12-19 13:03:52 +0300

    prjID=7&id=1134#3

--- App/Views/admin.php
+++ App/Views/admin.php
@@ -6,15 +6,12 @@
         <tr>
           <th rowspan="2" class="text-center">id</th>
           <th rowspan="2" class="text-center">МО</th>
-          <th colspan="2" class="text-center">Анкет на сайте</th>
           <th colspan="2" class="text-center">Анкет вручную</th>
           <th colspan="2" class="text-center">Анкет МЗ</th>
           <th colspan="2" class="text-center">Анкет всего</th>
           <th rowspan="2" class="text-center">Анкет всего</th>
         </tr>
         <tr>
-          <th class="text-center">Амбулатория</th>
-          <th class="text-center">Стационар</th>
           <th class="text-center">Амбулатория</th>
           <th class="text-center">Стационар</th>
           <th class="text-center">Амбулатория</th>
@@ -28,12 +25,10 @@
         <tr>
           <td><?php echo $ambulance->id; ?></td>
           <td><?php echo $ambulance->name; ?></td>
-          <td><?php echo $ambulance->ambulatoria->getCountByType('Сайт'); ?></td>
-          <td><?php echo $ambulance->stacionar->getCountByType('Сайт'); ?></td>
           <td><?php echo $ambulance->ambulatoria->getCountByType('Вручную'); ?></td>
           <td><?php echo $ambulance->stacionar->getCountByType('Вручную'); ?></td>
-          <td><?php echo $ambulance->ambulatoria->getCountByType('mz'); ?></td>
-          <td><?php echo $ambulance->stacionar->getCountByType('mz'); ?></td>
+          <td><?php echo $ambulance->ambulatoria->getCountByType('mz')+$ambulance->ambulatoria->getCountByType('Сайт'); ?></td>
+          <td><?php echo $ambulance->stacionar->getCountByType('mz')+$ambulance->stacionar->getCountByType('Сайт'); ?></td>
           <td><?php echo $ambulance->ambulatoria->getCount(); ?></td>
           <td><?php echo $ambulance->stacionar->getCount(); ?></td>
           <td><?php echo $ambulance->stacionar->getCount()+$ambulance->ambulatoria->getCount(); ?></td>
